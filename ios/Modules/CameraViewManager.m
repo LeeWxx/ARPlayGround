baa3e8e9495bec1426058206b6634346299bbcdf
LeeWxx <dnckd0903@naver.com>
2025-03-13 18:13:28 +0900

    feat: add CameraViewManager for integration

---
+++ ios/Modules/CameraViewManager.m
@@ -0,0 +1,11 @@
+#import <React/RCTViewManager.h>
+
+@interface RCT_EXTERN_MODULE(CameraViewManager, RCTViewManager)
+
+RCT_EXTERN_METHOD(capturePhoto:(nonnull NSNumber *)node
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(clearOverlay:(nonnull NSNumber *)node)
+
+@end 
